49af0685857a388c35c9af498daf9b3be8cc1df1
San-Panhavuth <panhavuth.san22@kit.edu.kh>
2024-11-08 09:54:33 +0700

    comment out read_group

--- devel_logistic_management/models/operation_shipment.py
+++ devel_logistic_management/models/operation_shipment.py
@@ -50,37 +50,37 @@ class OperationShipment(models.Model):
         stage_ids = stages._search([], order=order, access_rights_uid=SUPERUSER_ID)
         return stages.browse(stage_ids)
 
-    @api.model
-    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
-        """
-            Override read_group to calculate the sum of the non-stored fields that depend on the user context
-        """
-        res = super(OperationShipment, self).read_group(domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy)
-        shipments = self.env['operation.shipment']
-        for shipment in res:
-            if '__domain' in shipment:
-                shipments = self.search(shipment['__domain'])
-            if 'total_invoices' in fields:
-                shipment['total_invoices'] = sum(shipments.mapped('total_invoices'))
-            if 'tot_operation_expense' in fields:
-                shipment['tot_operation_expense'] = sum(shipments.mapped('tot_operation_expense'))
-            if 'balance' in fields:
-                shipment['balance'] = sum(shipments.mapped('balance'))
-            if 'total_cash_received' in fields:
-                shipment['total_cash_received'] = sum(shipments.mapped('total_cash_received'))
-            if 'total_expensed' in fields:
-                shipment['total_expensed'] = sum(shipments.mapped('total_expensed'))
-            if 'cash_balance' in fields:
-                shipment['cash_balance'] = sum(shipments.mapped('cash_balance'))
-            if 'total_deposit_amount' in fields:
-                shipment['total_deposit_amount'] = sum(shipments.mapped('total_deposit_amount'))
-            if 'balance_refund_amount' in fields:
-                shipment['balance_refund_amount'] = sum(shipments.mapped('balance_refund_amount'))
-            if 'refund_container_deposit_amount' in fields:
-                shipment['refund_container_deposit_amount'] = sum(shipments.mapped('refund_container_deposit_amount'))
-            if 'total_reimbursed' in fields:
-                shipment['total_reimbursed'] = sum(shipments.mapped('total_reimbursed'))
-        return res
+    # @api.model
+    # def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+    #     """
+    #         Override read_group to calculate the sum of the non-stored fields that depend on the user context
+    #     """
+    #     res = super(OperationShipment, self).read_group(domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy)
+    #     shipments = self.env['operation.shipment']
+    #     for shipment in res:
+    #         if '__domain' in shipment:
+    #             shipments = self.search(shipment['__domain'])
+    #         if 'total_invoices' in fields:
+    #             shipment['total_invoices'] = sum(shipments.mapped('total_invoices'))
+    #         if 'tot_operation_expense' in fields:
+    #             shipment['tot_operation_expense'] = sum(shipments.mapped('tot_operation_expense'))
+    #         if 'balance' in fields:
+    #             shipment['balance'] = sum(shipments.mapped('balance'))
+    #         if 'total_cash_received' in fields:
+    #             shipment['total_cash_received'] = sum(shipments.mapped('total_cash_received'))
+    #         if 'total_expensed' in fields:
+    #             shipment['total_expensed'] = sum(shipments.mapped('total_expensed'))
+    #         if 'cash_balance' in fields:
+    #             shipment['cash_balance'] = sum(shipments.mapped('cash_balance'))
+    #         if 'total_deposit_amount' in fields:
+    #             shipment['total_deposit_amount'] = sum(shipments.mapped('total_deposit_amount'))
+    #         if 'balance_refund_amount' in fields:
+    #             shipment['balance_refund_amount'] = sum(shipments.mapped('balance_refund_amount'))
+    #         if 'refund_container_deposit_amount' in fields:
+    #             shipment['refund_container_deposit_amount'] = sum(shipments.mapped('refund_container_deposit_amount'))
+    #         if 'total_reimbursed' in fields:
+    #             shipment['total_reimbursed'] = sum(shipments.mapped('total_reimbursed'))
+    #     return res
 
     name = fields.Char(default=lambda self: _('New'),
             copy=False, readonly=True)
